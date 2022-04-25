4c649efe97e5a7c5508103d1221d2e4b9476b0d1
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-04-25 21:01:52 +0200

    createWave and fetchWaves functions

--- contracts/Waver.sol
+++ contracts/Waver.sol
@@ -1,6 +1,67 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Waver {}
+contract Waver is Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _waveIds;
+
+    struct Wave {
+        uint256 id;
+        uint256 likes;
+        address author;
+        string content;
+    }
+
+    mapping(uint256 => Wave) public idToWave;
+    mapping(string => Wave) public hashToWave;
+
+    // events
+    event WaveCreated(uint256 id, address author, string content);
+    event WaveLiked(uint256 id, uint256 likes, address author);
+    event WaveUnliked(uint256 id, uint256 likes, address author);
+
+    constructor() {}
+
+    // create a new wave
+    function createWave(string memory _contentHash) public {
+        _waveIds.increment();
+        uint256 waveId = _waveIds.current();
+        Wave storage wave = idToWave[waveId];
+        wave.id = waveId;
+        wave.likes = 0;
+        wave.author = msg.sender;
+        wave.content = _contentHash;
+        hashToWave[_contentHash] = wave;
+
+        emit WaveCreated(waveId, wave.author, wave.content);
+    }
+
+    function fetchWaves() public view returns (Wave[] memory) {
+        uint256 wavesAmount = _waveIds.current();
+
+        Wave[] memory waves = new Wave[](wavesAmount);
+
+        for (uint256 i = 0; i < wavesAmount; i++) {
+            uint256 id = i + 1;
+            Wave storage wave = idToWave[id];
+            waves[i] = wave;
+        }
+
+        return waves;
+    }
+
+    // TODO
+    function like() public {}
+
+    // TODO
+    function unlike() public {}
+
+    // TODO
+    function deposit() public payable {
+        console.log("deposit");
+    }
+}
