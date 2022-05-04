5f2ad784a9274be40e4a1e9928750a1088393f0e
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-05-04 19:03:54 +0200

    navigation animation

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -7,18 +7,29 @@ import Mint from "./icons/mint.svg";
 import Wallet from "./icons/wallet.svg";
 
 const Navigation = () => {
+  const handleActive = (e) => {
+    console.log(e.target);
+    e.target.classList.add(styles.active);
+  };
+
   return (
     <div className={styles.container}>
       <nav className={styles.nav}>
-        <NavigationItem>
-          <Home />
-        </NavigationItem>
-        <NavigationItem active>
-          <Mint />
-        </NavigationItem>
-        <NavigationItem>
-          <Info />
-        </NavigationItem>
+        <NavigationItem
+          icon={<Home />}
+          title="Home"
+          handleActive={handleActive}
+        />
+        <NavigationItem
+          icon={<Mint />}
+          title="Mint"
+          handleActive={handleActive}
+        />
+        <NavigationItem
+          icon={<Info />}
+          title="Info"
+          handleActive={handleActive}
+        />
       </nav>
     </div>
   );
--- components/Navigation/NavigationItem.js
+++ components/Navigation/NavigationItem.js
@@ -1,9 +1,10 @@
 import styles from "./Navigation.module.scss";
 
-const NavigationItem = ({ active, children }) => {
+const NavigationItem = ({ icon, title, handleActive }) => {
   return (
-    <div className={`${styles.item} ${active && styles.active}`}>
-      {children}
+    <div className={styles.item} onClick={(e) => handleActive(e)}>
+      <p className={styles.title}>{title}</p>
+      <div className={styles.icon}>{icon}</div>
     </div>
   );
 };
